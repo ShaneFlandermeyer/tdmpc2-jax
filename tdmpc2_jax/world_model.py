434b03495f9e7cce7aa8f9c7fb7492663502ac27
Shane Flandermeyer <shaneflandermeyer@gmail.com>
2025-01-13 11:18:07 -0600

    >700 dog-run reward with correct logprobs

--- tdmpc2_jax/world_model.py
+++ tdmpc2_jax/world_model.py
@@ -272,8 +272,8 @@ class WorldModel(struct.PyTreeNode):
     # Sample action and compute logprobs
     eps = jax.random.normal(key, mean.shape)
     action = mean + eps * jnp.exp(log_std)
-    residual = (-0.5 * eps**2 - log_std).sum(-1)
-    log_probs = action.shape[-1] * (residual - 0.5 * jnp.log(2 * jnp.pi))
+    residual = (-0.5 * eps**2 - log_std)
+    log_probs = (residual - 0.5 * jnp.log(2 * jnp.pi)).sum(-1)
 
     # Squash tanh
     mean = jnp.tanh(mean)
