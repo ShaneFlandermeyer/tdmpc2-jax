Update plan function arguments, remove discount scheduling, fix off-by-one error in replay buffer

tdmpc2_jax/tdmpc2.py
@@ -89,7 +89,7 @@ class TDMPC2(struct.PyTreeNode):
   def act(self,
           obs: PyTree,
           prev_plan: Optional[Tuple[jax.Array, jax.Array]] = None,
-          train: bool = True,
+          deterministic: bool = True,
           *,
           key: PRNGKeyArray
           ) -> Tuple[np.ndarray, Optional[Tuple[jax.Array]]]:
@@ -97,20 +97,11 @@ class TDMPC2(struct.PyTreeNode):
     z = self.model.encode(obs, self.model.encoder.params, key=encoder_key)
 
     if self.mpc:
-      if prev_plan is None:
-        batch_dims = z.shape[:-1]
-        prev_plan = (
-            jnp.zeros((*batch_dims, self.horizon, self.model.action_dim)),
-            jnp.full(
-                (*batch_dims, self.horizon, self.model.action_dim),
-                self.max_plan_std
-            )
-        )
       action, plan = self.plan(
           z=z,
           horizon=self.horizon,
           prev_plan=prev_plan,
-          deterministic=not train,
+          deterministic=deterministic,
           key=action_key
       )
     else:
@@ -125,8 +116,8 @@ class TDMPC2(struct.PyTreeNode):
   def plan(self,
            z: jax.Array,
            horizon: int,
-           prev_plan: Tuple[jax.Array, jax.Array],
-           deterministic: bool,
+           prev_plan: Tuple[jax.Array, jax.Array] = None,
+           deterministic: bool = False,
            *,
            key: PRNGKeyArray,
            ) -> Tuple[jax.Array, Tuple[jax.Array, jax.Array]]:
@@ -193,8 +184,8 @@ class TDMPC2(struct.PyTreeNode):
     std = jnp.full(
         (*batch_shape, horizon, self.model.action_dim), self.max_plan_std
     )
-    mean = mean.at[..., :-1, :].set(prev_plan[0][..., 1:, :])
-    std = std.at[..., :-1, :].set(prev_plan[1][..., 1:, :])
+    if prev_plan is not None:
+      mean = mean.at[..., :-1, :].set(prev_plan[0][..., 1:, :])
 
     for i in range(self.mppi_iterations):
       actions = actions.at[..., self.policy_prior_samples:, :, :].set(

tdmpc2_jax/train.py
@@ -201,7 +201,7 @@ def train(cfg: dict):
     ##############################
     ep_count = np.zeros(env_config.num_envs, dtype=int)
     prev_logged_step = global_step
-    prev_plan = None
+    plan = None
     observation, _ = env.reset(seed=cfg.seed)
 
     T = 500
@@ -218,15 +218,11 @@ def train(cfg: dict):
         expert_mean, expert_std = action, np.ones_like(action)
       else:
         rng, action_key = jax.random.split(rng)
-        if prev_plan is not None:
-          prev_plan = (
-              prev_plan[0], jnp.full_like(prev_plan[1], agent.max_plan_std)
-          )
-        action, prev_plan = agent.act(
-            observation, prev_plan=prev_plan, train=True, key=action_key
+        action, plan = agent.act(
+            observation, prev_plan=plan, deterministic=False, key=action_key
         )
-        expert_mean = prev_plan[0][..., 0, :]
-        expert_std = prev_plan[1][..., 0, :]
+        expert_mean = plan[0][..., 0, :]
+        expert_std = plan[1][..., 0, :]
 
       next_observation, reward, terminated, truncated, info = env.step(action)
 
@@ -250,10 +246,10 @@ def train(cfg: dict):
       # Handle terminations/truncations
       done = np.logical_or(terminated, truncated)
       if np.any(done):
-        if prev_plan is not None:
-          prev_plan = (
-              prev_plan[0].at[done].set(0),
-              prev_plan[1].at[done].set(agent.max_plan_std)
+        if plan is not None:
+          plan = (
+              plan[0].at[done].set(0),
+              plan[1].at[done].set(agent.max_plan_std)
           )
         for ienv in range(env_config.num_envs):
           if done[ienv]:
@@ -304,20 +300,14 @@ def train(cfg: dict):
             rng, reanalyze_key = jax.random.split(rng)
             b = bmpc_config.reanalyze_batch_size
             h = bmpc_config.reanalyze_horizon
-            _, plan_dist = agent.plan(
+            _, reanalyzed_plan = agent.plan(
                 z=true_zs[:, :b, :],
                 horizon=h,
-                prev_plan=(
-                    jnp.zeros((b, h, agent.model.action_dim)),
-                    jnp.full(
-                        (b, h, agent.model.action_dim), agent.max_plan_std
-                    )
-                ),
                 deterministic=True,
                 key=reanalyze_key
             )
-            reanalyze_mean = plan_dist[0][..., 0, :]
-            reanalyze_std = plan_dist[1][..., 0, :]
+            reanalyze_mean = reanalyzed_plan[0][..., 0, :]
+            reanalyze_std = reanalyzed_plan[1][..., 0, :]
             # Update expert policy in buffer
             # Reshape for buffer: (T, B, A) -> (B, T, A)
             env_inds = batch_env_inds[:b, None]
@@ -331,15 +321,10 @@ def train(cfg: dict):
 
             # Update policy with reanalyzed samples
             if not pretrain:
-              # Linearly increase BMPC discount over training
-              bmpc_discount = bmpc_config.min_discount + \
-                  (global_step / cfg.max_steps) * \
-                  (bmpc_config.max_discount - bmpc_config.min_discount)
-              reanalyze_age = total_reanalyze_steps - batch['last_reanalyze']
-              bmpc_scale = bmpc_discount**reanalyze_age
-
               batch['expert_mean'][:, :b, :] = reanalyze_mean
               batch['expert_std'][:, :b, :] = reanalyze_std
+              reanalyze_age = total_reanalyze_steps - batch['last_reanalyze']
+              bmpc_scale = bmpc_config.discount**reanalyze_age
               rng, policy_key = jax.random.split(rng)
               agent, policy_info = agent.update_policy(
                   zs=true_zs,